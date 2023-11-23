6c98c47ba3e6343668630d926798a77b37d29ef7
Rafid-Rahman <rafidrahman00@gmail.com>
2023-11-23 20:10:30 +0600

    Portfolio

---
+++ Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            ViewBag.Message = "Educational Qualifications Information.";
+
+            return View();
+        }
+
+        public ActionResult Project()
+        {
+            ViewBag.Message = "Projects listed below!";
+
+            return View();
+        }
+
+        public ActionResult Reference()
+        {
+            ViewBag.Message = "Reference list!";
+
+            return View();
+        }
+
+        public ActionResult Personal_info()
+        {
+            ViewBag.Message = "Here is my personal information provided!";
+
+            return View();
+        }
+    }
+}
